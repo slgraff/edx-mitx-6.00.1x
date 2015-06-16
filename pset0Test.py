19421a4a3bcb54a443a82804415b9fce17a58d00
Steve Graff <slgraff@gmail.com>
2015-06-16 15:33:21 -0400

    Problem Set 0
    Problem Set 0 from edX MITx 6.00.1x Introduction to Computer Science
    and Programming Using Python

---
+++ pset0Test.py
@@ -0,0 +1,7 @@
+# edX MITx 6.00.1x 
+# Introduction to Computer Science and Programming Using Python
+# Problem Set 0
+# Installation of Canopy Python IDE and hello world project
+
+print 'hello world'
+print 'I like 6.00.1x!'
