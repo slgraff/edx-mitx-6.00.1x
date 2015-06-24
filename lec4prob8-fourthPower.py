854f369a3c26f9c4ef2cfeffba686a345e137aef
Steve Graff <slgraff@gmail.com>
2015-06-23 22:21:12 -0400

    Initial commit to GitHub

---
+++ lec4prob8-fourthPower.py
@@ -0,0 +1,18 @@
+# lec4prob8
+# Lecture 4, Problem 8
+
+##Write a Python function, fourthPower, that takes in one number and
+##returns that value raised to the fourth power.
+##
+##You should use the square procedure that you defined in Problem 3 of
+##these lecture exercises (you don't need to redefine square in this box;
+##when you call square, the tutor will use our definition).
+##
+##This function takes in one number and returns one number.
+
+def fourthPower(x):
+    '''
+    x: int or float.
+    '''
+    # Your code here
+
